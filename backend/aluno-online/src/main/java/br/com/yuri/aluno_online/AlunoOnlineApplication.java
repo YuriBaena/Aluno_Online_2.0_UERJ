5a6ca8b579accf7183caa04c44c6688b387d4f9a
Yuri Baena Dias Nascimento <yuri.baena@globalweb.com.br>
2025-12-21 17:56:16 -0300

    Inicializando backend, banco de dados e docker

---
+++ backend/aluno-online/src/main/java/br/com/yuri/aluno_online/AlunoOnlineApplication.java
@@ -0,0 +1,13 @@
+package br.com.yuri.aluno_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunoOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunoOnlineApplication.class, args);
+	}
+
+}
---
+++ backend/aluno-online/src/main/java/br/com/yuri/aluno_online/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package br.com.yuri.aluno_online.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable()) // Desativa CSRF para facilitar testes
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll() // Libera TODOS os endpoints
+            );
+        return http.build();
+    }
+}
