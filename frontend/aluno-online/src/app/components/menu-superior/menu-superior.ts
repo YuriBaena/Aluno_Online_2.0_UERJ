e86d3c33281cb6d5ffed77222e7b7229d5b08d5e
Yuri Baena Dias Nascimento <yuri.baena@globalweb.com.br>
2025-12-30 12:08:12 -0300

    feat: Implement user profile dropdown and menu superior component
    - Added user profile dropdown in the Aluno component with logout functionality.
    - Created styles for the user profile and dropdown menu.
    - Developed a Menu Superior component that includes a logo, search bar, and user controls.
    - Removed old Aluno page implementation and integrated the new components into the Home page.
    - Updated global styles for better layout and consistency.
    - Added Font Awesome for icon usage in the application.

---
+++ frontend/aluno-online/src/app/components/menu-superior/menu-superior.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Aluno } from '../aluno/aluno';
+
+@Component({
+  selector: 'app-menu-superior',
+  imports: [Aluno],
+  templateUrl: './menu-superior.html',
+  styleUrl: './menu-superior.scss',
+})
+export class MenuSuperior {
+
+}
