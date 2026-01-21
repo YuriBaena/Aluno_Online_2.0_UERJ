de7d9275f2a9dbd973c64e1dd0dcacce06a9c00e
Yuri Baena Dias Nascimento <yuri.baena@globalweb.com.br>
2026-01-21 12:35:36 -0300

    Test

--- frontend/aluno-online/src/app/services/my-cronograma.spec.ts
+++ frontend/aluno-online/src/app/services/my-cronograma.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { MyCronogramaService } from './my-cronograma';
 
 describe('MyCronogramaService', () => {
   let service: MyCronogramaService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        MyCronogramaService,
+        provideHttpClient(),        // Adicione isso
+        provideHttpClientTesting() // Adicione isso
+      ]
+    });
     service = TestBed.inject(MyCronogramaService);
   });
 
