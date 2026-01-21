e263b005750d26ee2b953b498787c012583b9801
Yuri Baena Dias Nascimento <yuri.baena@globalweb.com.br>
2026-01-21 12:33:10 -0300

    Test
    Ajeita teste frontend

--- frontend/aluno-online/src/app/pages/my-cronograma/my-cronograma.spec.ts
+++ frontend/aluno-online/src/app/pages/my-cronograma/my-cronograma.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { MyCronograma } from './my-cronograma';
 
 describe('MyCronograma', () => {
@@ -8,7 +9,11 @@ describe('MyCronograma', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MyCronograma]
+      imports: [MyCronograma], 
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
     })
     .compileComponents();
 
