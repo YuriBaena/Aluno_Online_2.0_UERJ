08599cb02b9f8553b77a9672a0295e40f377d046
Yuri Baena Dias Nascimento <yuri.baena@globalweb.com.br>
2026-02-17 20:48:05 -0300

    Teste
     - Corrigindo testes

--- frontend/aluno-online/src/app/pages/meta-academica/meta-academica.spec.ts
+++ frontend/aluno-online/src/app/pages/meta-academica/meta-academica.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MetaAcademica } from './meta-academica';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('MetaAcademica', () => {
   let component: MetaAcademica;
@@ -8,7 +10,11 @@ describe('MetaAcademica', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MetaAcademica]
+      imports: [MetaAcademica],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ]
     })
     .compileComponents();
 
--- frontend/aluno-online/src/app/services/objetivo.spec.ts
+++ frontend/aluno-online/src/app/services/objetivo.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { ObjetivoService } from './objetivo';
 
-describe('Objetivo', () => {
+describe('ObjetivoService', () => {
   let service: ObjetivoService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        ObjetivoService,           // Provide the service itself
+        provideHttpClient(),       // Provide the actual HttpClient
+        provideHttpClientTesting() // Provide the testing backend to mock requests
+      ]
+    });
     service = TestBed.inject(ObjetivoService);
   });
 
